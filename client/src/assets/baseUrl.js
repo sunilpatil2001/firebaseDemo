baabb64f35d80d490589c02db6c9cab1cac2e051
= <=>
2024-10-25 15:39:18 +0530

    demo

--- client/src/assets/baseUrl.js
+++ client/src/assets/baseUrl.js
@@ -1,4 +1,5 @@
 //const baseUrl = 'https://psychiatrist-7kv8.onrender.com'
-const baseUrl ='https://finalmigrate.onrender.com'
+// const baseUrl ='https://finalmigrate.onrender.com'
+const baseUrl ='https://backend-sansun.web.app'
 // const baseUrl = 'http://localhost:3001'
 export default baseUrl
